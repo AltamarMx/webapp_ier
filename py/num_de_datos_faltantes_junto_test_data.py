b611192ffad7507879c9cb8217ff0bef3bee1678
roelej <roelej@ier.unam.mx>
2025-03-09 23:37:14 -0600

    Le añadi el tipado al script de num_datos_faltantes en pruebas

--- py/num_de_datos_faltantes_junto_test_data.py
+++ py/num_de_datos_faltantes_junto_test_data.py
@@ -3,27 +3,30 @@ import pandas as pd
 import numpy as np
 from glob import glob
 # %%
-def proporcion_faltantes(f):
-    data = pd.read_csv(
-    f,
+def importar_datos(path: str) -> pd.DataFrame:
+    data_frame = pd.read_csv(
+    path,
     skiprows=[0,2,3],
     index_col=0,
     parse_dates=True,
     dayfirst=True,
     )
-    del data["RECORD"]
+    del data_frame["RECORD"]
+    return data_frame
+
+def proporcion_faltantes(data_frame: pd.DataFrame) -> float:
     # Eliminar la primera fila si está completamente vacía
-    if data.iloc[0].isna().all():
-        data = data.iloc[1:]
+    if data_frame.iloc[0].isna().all():
+        data_frame = data_frame.iloc[1:]
 
     # Asegurarse de que el índice es de tipo datetime
-    data.index = pd.to_datetime(data.index)
+    data_frame.index = pd.to_datetime(data_frame.index)
 
     # Eliminar duplicados en el índice
-    data = data.loc[~data.index.duplicated(keep='first')]
+    data_frame = data_frame.loc[~data_frame.index.duplicated(keep='first')]
 
     # Obtener el año del primer dato (asumiendo que todos los datos son del mismo año)
-    year = data.index.year[0]
+    year = data_frame.index.year[0]
 
     # Crear el rango de fechas completo para ese año
     start_date = f'{year}-01-01 00:00:00'
@@ -31,34 +34,15 @@ def proporcion_faltantes(f):
     full_index = pd.date_range(start=start_date, end=end_date, freq='H')
 
     # Reindexar para llenar los huecos con NaN
-    data = data.reindex(full_index)
+    data_frame = data_frame.reindex(full_index)
     #Funcion para encontrar la proporcion de daltos faltantes
-    total_filas = len(data)
-    faltantes = data.isna().sum()  # Por columna
+    total_filas = len(data_frame)
+    faltantes = data_frame.isna().sum()  # Por columna
     proporcion_faltantes = faltantes / total_filas
-    return proporcion_faltantes
-    #return print(proporcion_faltantes)
 
-# %%
-paths = glob("../data/001_raw/RUOA/Datos_hora/*")
-#Se eliminan las rutas de los ultimos 3 archivos ya que tienen un 
-#error con el encoding y eso es otro ejercicio
-paths = paths[:-3]
-paths
+    #Aca se determina si es recomendable imputar los datos
 
-# %%
-resultados = []
-
-# Bucle for para aplicar la función a cada archivo y guardar los resultados
-for path in paths:
-    resultado = proporcion_faltantes(path)  # Llama a la función
-    resultados.append(resultado)  # Guarda el resultado en la lista
-
-# %%
-print('''Archivo   Porcentaje_faltantes   Recomendacion''')
-for i in range(len(paths)):
-    archivo = paths[i][-16:-4]
-    porcentaje = round(resultados[i].max()*100,2)
+    porcentaje = round(proporcion_faltantes.max()*100,2)
     if porcentaje < 5:
         recomendacion = "Es seguro imputar"
     elif porcentaje < 20:
@@ -66,6 +50,17 @@ for i in range(len(paths)):
     elif porcentaje < 40:
         recomendacion = "Revisar si las faltas son aleatorias"
     else:
-        recomendacion = "Mejor no"
-    print(f'{archivo}   {porcentaje}   {recomendacion}')
+        recomendacion = "Mejor no imputar"
+    print(f'Tenemos {porcentaje}% de datos faltantes, podemos recomendar {recomendacion}')
+    return porcentaje, recomendacion
+    #return print(proporcion_faltantes)
 
+# %%
+paths = glob("../data/001_raw/RUOA/Datos_hora/*")
+#Se eliminan las rutas de los ultimos 3 archivos ya que tienen un 
+#error con el encoding y eso es otro ejercicio
+paths = paths[:-4]
+paths
+
+# %%
+proporcion_faltantes(importar_datos(paths[0]))
